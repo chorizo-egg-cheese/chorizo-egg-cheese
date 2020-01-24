3f5dbc933df18086954714d3a67b6edceedea8df
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-01-24 11:44:55 -0600

    worked on blog

--- pod-work.js
+++ pod-work.js
@@ -18,24 +18,24 @@
 // waitForMe(3000).then(() => console.log('3 seconds have passed'));
 // waitForMe(9000).then(() => console.log('9 seconds have passed'));
 
-$(document).ready(function () {
-    function pageLoad() {
-        $.ajax('data/blog.json')
-
-            .done(function (data) {
-                for (let i = 0; i < data.length; i++) {
-                    console.log(data);
-                    $('#posts').append(
-                        "<h1></h1>" + data[i].title +
-                        "<p></p>" + data[i].content +
-                        "<p></p>" + data[i].categories +
-                        "<p></p>" + data[i].date
-                    )
-
-                }
-
-            })
-    }
-    pageLoad()
-    $(#refresh)
-})
+// $(document).ready(function () {
+//     function pageLoad() {
+//         $.ajax('data/blog.json')
+//
+//             .done(function (data) {
+//                 for (let i = 0; i < data.length; i++) {
+//                     console.log(data);
+//                     $('#post').html(
+//                         "<h1></h1>" + data[i].title +
+//                         "<p></p>" + data[i].content +
+//                         "<p></p>" + data[i].categories +
+//                         "<p></p>" + data[i].date
+//                     )
+//
+//                 }
+//
+//             })
+//     }
+//     pageLoad()
+//     $(#refresh)
+// })
